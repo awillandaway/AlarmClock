02be815ec0f2f1389b56565de8e294c2980067ac
William M. Stewart III <wmstewart.iii@gmail.com>
2018-12-06 15:46:14 -0500

    Updated JSDoc comments and updated getCurrentTime to actually work in IE due to weird indexOf issue

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,7 @@
 /**
  * Generic render function which sets a node's innerHTML to the provided template
- * @param {*} template An element/template to render in the provided node
- * @param {*} node The node to render the template in
+ * @param {HTMLElement} template An element/template to render in the provided node
+ * @param {HTMLElement} node The node to render the template in
  */
 export function render(template, node) {
 	if (!node) {
@@ -22,14 +22,23 @@ export function getCurrentDate() {
 
 /** Returns the current time as a string with padding*/
 export function getCurrentTime() {
-  let currentTime = new Date().toLocaleTimeString();
-  return currentTime.substring(0, currentTime.indexOf(':')) < 10 ? `0${currentTime}` : currentTime;
+  let now = new Date();
+  let hours = now.getHours(), AMorPM = "AM";
+  // convert to 12-hour format
+  if (hours > 12) {
+    hours = hours - 12;
+    AMorPM = "PM";
+  }
+  // make sure to pad all numbers if necessary
+  return `${padNumber(hours)}:${padNumber(now.getMinutes())}:${padNumber(now.getSeconds())} ${AMorPM}`
+  //one-line solution for padding that was working in all browsers except IE
+  //return currentTime.substring(0, currentTime.indexOf(':')) < 10 ? `0${currentTime}` : currentTime;
 }
 
 /**
- * Generate an array (of length len) of sequential numbers starting from the startIndex
- * @param {*} start
- * @param {*} end
+ * Generate an array (of length len) of sequential numbers starting from the start parameter
+ * @param {number} start Number to start the array at
+ * @param {number} end Number to end the array at
  */
 export function generateRangeArray(start, end) {
   let array = [];
@@ -41,8 +50,8 @@ export function generateRangeArray(start, end) {
 
 /**
  * Returns a new Option element with the specified label and value. Defaults to using the label as the value
- * @param {*} label
- * @param {*} value 
+ * @param {string} label Label of the option element
+ * @param {string} value Value  of the option element. Defaults to label
  */
 export function generateOptionElement(label, value = label) {
   return new Option(label, value);
@@ -50,7 +59,7 @@ export function generateOptionElement(label, value = label) {
 
 /**
  * Pads a number with a leading 0 if the number is less than 10, to mimic how numbers are displayed on digital clocks
- * @param {*} number 
+ * @param {number} number 
  */
 export function padNumber(number) {
   if (number < 10) {
